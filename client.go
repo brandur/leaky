10163b109e580a086c4d5f97ed4395626b08873b
Brandur <brandur@mutelight.org>
2013-11-29 21:40:29 -0800

    Refactor most send/receive logic into Client

--- client.go
+++ client.go
@@ -2,22 +2,31 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
 type Client struct {
+	PeerSet *PeerSet
 	Url string
 }
 
-func newClient(url string) *Client {
+func newClient(peerSet *PeerSet, url string) *Client {
 	return &Client{
+		PeerSet: peerSet,
 		Url: url,
 	}
 }
 
-func (c *Client) connectAndSend(path string, data []byte, handler func([]byte)) {
+func (c *Client) buildHandler(handler func(*Client, []byte)) func([]byte) {
+	return func(responseData []byte) {
+		handler(c, responseData)
+	}
+}
+
+func (c *Client) send(path string, data []byte, handler func([]byte)) {
 	buffer := bytes.NewBuffer(data)
 	r, err := http.Post(c.Url+path, "application/json", buffer)
 	if err != nil {
@@ -31,4 +40,36 @@ func (c *Client) connectAndSend(path string, data []byte, handler func([]byte)) 
 	}
 
 	handler(responseData)
+}
+
+func handleAppendEntriesResponse(c *Client, responseData []byte) {
+	response := AppendEntriesResponse{}
+	if err := json.Unmarshal(responseData, &response); err != nil {
+		panic(err)
+	}
+	c.PeerSet.AppendEntriesResponseChan <- response
+}
+
+func handleRequestVoteResponse(c *Client, responseData []byte) {
+	response := RequestVoteResponse{}
+	if err := json.Unmarshal(responseData, &response); err != nil {
+		panic(err)
+	}
+	c.PeerSet.RequestVoteResponseChan <- response
+}
+
+func (c *Client) sendAppendEntriesRequest(request *AppendEntriesRequest) {
+	requestData, err := json.Marshal(request)
+	if err != nil {
+		panic(err)
+	}
+	c.send("/append-entries", requestData, c.buildHandler(handleAppendEntriesResponse))
+}
+
+func (c *Client) sendRequestVoteRequest(request *RequestVoteRequest) {
+	requestData, err := json.Marshal(request)
+	if err != nil {
+		panic(err)
+	}
+	c.send("/request-vote", requestData, c.buildHandler(handleRequestVoteResponse))
 }
--- peer_set.go
+++ peer_set.go
@@ -1,15 +1,11 @@
 package main
 
-import (
-	"encoding/json"
-)
-
 type PeerSet struct {
-	set                       []Client
 	AppendEntriesRequestChan  chan AppendEntriesRequest
 	AppendEntriesResponseChan chan AppendEntriesResponse
 	RequestVoteRequestChan    chan RequestVoteRequest
 	RequestVoteResponseChan   chan RequestVoteResponse
+	set                       []*Client
 }
 
 func newPeerSet() *PeerSet {
@@ -18,35 +14,17 @@ func newPeerSet() *PeerSet {
 		AppendEntriesResponseChan: make(chan AppendEntriesResponse, 50),
 		RequestVoteRequestChan:    make(chan RequestVoteRequest, 50),
 		RequestVoteResponseChan:   make(chan RequestVoteResponse, 50),
+		set:                       make([]*Client, 0, 100),
 	}
 }
 
-func (p *PeerSet) broadcast(path string, data []byte, handler func([]byte)) {
-	for i := range p.set {
-		go p.set[i].connectAndSend(path, data, handler)
+func (p *PeerSet) addClient(url string) {
+	// grow into cap by one element and insert the new client there
+	p.set = p.set[:len(p.set)+1]
+	p.set[len(p.set)] = &Client{
+		PeerSet: p,
+		Url: url,
 	}
-}
-
-func (p *PeerSet) buildHandler(handler func(*PeerSet, []byte)) func([]byte) {
-	return func(responseData []byte) {
-		handler(p, responseData)
-	}
-}
-
-func handleAppendEntriesResponse(p *PeerSet, responseData []byte) {
-	response := AppendEntriesResponse{}
-	if err := json.Unmarshal(responseData, &response); err != nil {
-		panic(err)
-	}
-	p.AppendEntriesResponseChan <- response
-}
-
-func handleRequestVoteResponse(p *PeerSet, responseData []byte) {
-	response := RequestVoteResponse{}
-	if err := json.Unmarshal(responseData, &response); err != nil {
-		panic(err)
-	}
-	p.RequestVoteResponseChan <- response
 }
 
 func (p *PeerSet) len() int {
@@ -57,18 +35,14 @@ func (p *PeerSet) run() {
 	for {
 		select {
 		case request := <-p.AppendEntriesRequestChan:
-			requestData, err := json.Marshal(request)
-			if err != nil {
-				panic(err)
+			for i := range p.set {
+				go p.set[i].sendAppendEntriesRequest(&request)
 			}
-			p.broadcast("/append-entries", requestData, p.buildHandler(handleAppendEntriesResponse))
 
 		case request := <-p.RequestVoteRequestChan:
-			requestData, err := json.Marshal(request)
-			if err != nil {
-				panic(err)
+			for i := range p.set {
+				go p.set[i].sendRequestVoteRequest(&request)
 			}
-			p.broadcast("/request-vote", requestData, p.buildHandler(handleRequestVoteResponse))
 		}
 	}
 }
